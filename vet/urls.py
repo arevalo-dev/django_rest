64f872f437ebd46a0824fe76c15b04eed03e62a3
arevalo-dev <arevalo-dev@hotmail.com>
2021-08-02 20:52:32 -0500

    feat: CRUD PET

--- vet/urls.py
+++ vet/urls.py
@@ -6,6 +6,6 @@ app_name = 'vet'
 urlpatterns = [
     path("owners/", PetOwnersListCreate.as_view(), name="owners_list-create"),
     path("owners/<int:pk>", PetOwnerRetrieveUpdateDestroyAPIView.as_view(), name="owners_retrieve-update-destroy"),
-    path("pets/", PetsListCreate.as_view(), name="pets_list"),
+    path("pets/", PetsListCreate.as_view(), name="pets_list-create"),
     path("pets/<int:pk>", PetsRetrieveUpdateDestroyAPIView.as_view(), name="pets_list"),
 ]
